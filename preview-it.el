21a921a82c3f2cb9b49330e3df20fbd34696e6ce
Jen-Chieh <jcs090218@gmail.com>
2020-10-29 12:41:30 +0800

    Added commentary.

--- preview-it.el
+++ preview-it.el
@@ -27,7 +27,16 @@
 
 ;;; Commentary:
 ;;
-;; Preview anything at point.
+;; This package is to attempt to preview any data as possible.  Data can
+;; refers to `file', `image', or `url'.
+;;
+;; You may enabled this package by doing the following execution.
+;;
+;;   `(preview-it-mode 1)`
+;;
+;; Or you can either enabled it globally by doing this.
+;;
+;;   `(global-preview-it-mode 1)`
 ;;
 
 ;;; Code:
